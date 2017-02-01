ebf8381e38d3b4ab5e5a238cd9fbbca27a80b218
Juan Ramirez <ep.juan@gmail.com>
2017-01-31 20:07:11 -0500

    Publicación Plan de Acción 2017

--- dist/page.Planes.php
+++ dist/page.Planes.php
@@ -100,6 +100,19 @@
 
 		<div class="col-lg-12">
 			<div class="list-group">
+				<a href="https://drive.google.com/file/d/0B88B_ynqeND5Q1RpUVVZQW5Za3M/view?usp=sharing" target="_blank" class="list-group-item">
+				<h4 class="list-group-item-heading">Plan de Acción 2017</h4>
+				<p class="list-group-item-text">
+				Plan de Acción Institucional para el 2017. <br>
+				<hr>
+				<small>Publicado en Enero 31 de 2017</small>
+				</p>
+				</a>
+			</div>			
+		</div>
+
+		<div class="col-lg-12">
+			<div class="list-group">
 				<a href="https://drive.google.com/file/d/0B88B_ynqeND5VmY1aXIzTWVWLW8/view?usp=sharing" target="_blank" class="list-group-item">
 				<h4 class="list-group-item-heading">Plan de Acción 2016</h4>
 				<p class="list-group-item-text">
