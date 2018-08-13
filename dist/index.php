259f41fd523980aea448a5e2ada39da886f1f2ba
Juan Ramirez <ep.juan@gmail.com>
2018-08-13 11:42:34 -0500

    Eliminación Código Google Analitycs y Script de Google Maps

--- dist/index.php
+++ dist/index.php
@@ -27,7 +27,6 @@ if (isset($_GET['menu'])){
 
         <!-- JAVASCRIPT -->
 
-        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true"></script>
         <script src="https://code.jquery.com/jquery.js"></script>
         <!--<script src="js/highcharts.js"></script>-->
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -226,24 +225,6 @@ if (isset($_GET['menu'])){
                 </div>
             </div>
         </footer>
-
-    <!-- Google Analytics -->
-
-    <script>
-
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-37716121-1']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
-
-
-    </script>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 
