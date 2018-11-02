d598725dc7ed50885b2508ae7edc9224d3b9c23c
Juan Ramirez <ep.juan@gmail.com>
2018-11-01 19:32:02 -0500

    Update page.Informes.php

--- dist/page.Informes.php
+++ dist/page.Informes.php
@@ -24,7 +24,7 @@
 			</div>
 			
 			<div class="list-group">
-				<a href="https://drive.google.com/file/d/13Xce9517pQBOBQLRy-XbLNmOqlaaZsCu/view?usp=sharing" target="_blank" class="list-group-item">
+				<a href="https://drive.google.com/file/d/13lDxliBazedJDWgFQ4MZUk9H8Wd6Glpt/view?usp=sharing" target="_blank" class="list-group-item">
 				<h4 class="list-group-item-heading">Estados Financieros Diciembre 2017</h4>
 				<p class="list-group-item-text">
 				<hr>
